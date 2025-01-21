9b58bf186e4ca3c6a436ccb34710ad2ba3c980b3
olegkiprik <oleg.kiprik@proton.me>
2025-01-21 12:35:15 +0100

    Add site percolation threshold visualization

---
+++ proj2/c/site_square_topdown/main.c
@@ -0,0 +1,89 @@
+#include <glad.h>  
+#include <GLFW/glfw3.h> 
+#include <stdlib.h>     
+#include "update_callback.h"
+
+int main() {
+    uint32_t* pixels;
+    GLFWwindow* window;
+
+    pixels = malloc(sizeof(uint32_t) * WORLD_WIDTH * WORLD_HEIGHT);
+    if (!pixels) {
+        return 1;
+    }
+
+    if (!glfwInit()) {
+        free(pixels);
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwSwapInterval(1);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    window = glfwCreateWindow(WORLD_WIDTH, WORLD_HEIGHT, "Nice Simulation", glfwGetPrimaryMonitor(), NULL);
+    if (!window) {
+        goto FAILED;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        goto FAILED;
+    }
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, WORLD_WIDTH, WORLD_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    GLuint fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex, 0);
+
+    /* simulation context */
+    struct Context cx;
+    
+    if (!init_callback(pixels,  &cx)) {
+        goto FAILED;
+    }
+
+    int pause = 0;
+
+    while (!glfwWindowShouldClose(window)) {
+        if (!update_callback(pause, pixels, &cx)) {
+            goto FAILED;
+        }
+
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WORLD_WIDTH, WORLD_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+        glBlitFramebuffer(0, 0, WORLD_WIDTH, WORLD_HEIGHT, 0, 0, WORLD_WIDTH, WORLD_HEIGHT, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        if (cx.level == PERC_MAX_LVL) {
+            break;
+        }
+        if (glfwGetKey(window, GLFW_KEY_DELETE) == GLFW_PRESS) {
+            break;
+        }
+        if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) {
+            pause = 1;
+        }
+        if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
+            pause = 0;
+        }
+    }
+
+    finish_callback(pixels, &cx);
+
+FAILED:
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    free(pixels);
+    return 0;
+}
---
+++ proj2/c/site_square_topdown/update_callback.c
@@ -0,0 +1,249 @@
+#include "update_callback.h"
+#include <union_find_aos.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define BLOCKED_WIDTH (WORLD_WIDTH / 5)
+
+void assign_3_4(uint32_t* x, uint32_t y) {
+    *x = y;
+}
+
+#ifdef PP_UNIX
+void* thread_func(void* void_args) {
+    ThreadArgs* args = void_args;
+    size_t first = args->first;
+    size_t last_exc = first + args->n;
+    uint32_t* restrict color = args->cx->color;
+    uint32_t* restrict pixels = args->pixels;
+    DsufNode* uf = args->cx->uf;
+    while (1) {
+        if (sem_wait(&args->semaphore_in) == -1) {
+            return NULL;
+        }
+        if (args->finish) {
+            return NULL;
+        }
+        uint32_t* px = &pixels[first];
+        DsufNode* ufp = &uf[first];
+        for (; px < pixels+last_exc; ++px, ++ufp) {
+            DsufNode* node = ds_lazy_find(ufp); 
+            uint32_t* restrict pixel = px;
+            uint32_t* restrict col = &color[node - uf];
+            assign_3_4(pixel, *col);
+        }
+        (void)sem_post(&args->semaphore_out);
+    }
+    return NULL;
+}
+#endif
+
+int init_callback(uint32_t* pixels, struct Context* cx) {
+    const size_t width = WORLD_WIDTH;
+    const size_t height = WORLD_HEIGHT;
+    cx->site_prob = malloc(sizeof(float)*width*height);
+    cx->color = malloc(width*height*4);
+    cx->uf = calloc(width*height, sizeof(DsufNode));
+    cx->level = 0;
+
+#ifdef PP_UNIX
+    memset(cx->thread, 0, THREAD_COUNT * sizeof(pthread_t));
+#endif
+
+    float* site_prob = cx->site_prob;
+    uint32_t* color = cx->color;
+    DsufNode* uf = cx->uf;
+
+    if (!site_prob || !uf || !color) {
+        goto FAILED;
+    }
+
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            site_prob[x+y*width] = (float)rand()/RAND_MAX;
+            uf[x+y*width].parent = &uf[x+y*width];
+            color[x+y*width] = 0xff000000;
+        }
+    }
+    
+    for (size_t x = 0; x < WORLD_WIDTH; ++x) {
+        color[x+0*width] = 0xffff0000;
+        color[x+(WORLD_HEIGHT-1)*width] = 0xffffffff;
+        site_prob[x+0*width] = 0;
+        site_prob[x+(WORLD_HEIGHT-1)*width] = 0;
+    }
+
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < BLOCKED_WIDTH; ++x) {
+            site_prob[x+y*width] = 1;
+        }
+
+        for (size_t x = WORLD_WIDTH - BLOCKED_WIDTH; x < WORLD_WIDTH; ++x) {
+            site_prob[x+y*width] = 1;
+        }
+    }
+
+#ifdef PP_UNIX
+    size_t n = width*height;
+    const size_t chunk_size = n / (THREAD_COUNT+1);
+
+    ThreadArgs* thread_args = cx->thread_args;
+    for (size_t i = 0; i < THREAD_COUNT; ++i) {
+        thread_args[i].first = chunk_size * i;
+        thread_args[i].n = chunk_size;
+        thread_args[i].pixels = pixels;
+        thread_args[i].cx = cx;
+
+        thread_args[i].finish = 0;
+        (void)sem_init(&thread_args[i].semaphore_in, 0, 0);
+        (void)sem_init(&thread_args[i].semaphore_out, 0, 0);
+        if (pthread_create(&cx->thread[i], NULL, thread_func, &thread_args[i])) {
+            goto FAILED;
+        }
+    }
+#endif
+
+    return 1;
+FAILED:
+    finish_callback(pixels, cx);
+    return 0;
+}
+
+int finish_callback(uint32_t* pixels, struct Context* cx) {
+#ifdef PP_UNIX
+    for (size_t i = 0; i < THREAD_COUNT; ++i) {
+        cx->thread_args[i].finish = 1;
+        (void)sem_post(&cx->thread_args[i].semaphore_in);
+        (void)pthread_join(cx->thread[i], NULL);
+        (void)sem_destroy(&cx->thread_args[i].semaphore_in);
+        (void)sem_destroy(&cx->thread_args[i].semaphore_out);
+    }
+#endif
+
+    free(cx->site_prob);
+    free(cx->uf);
+    free(cx->color);
+    return 1;
+}
+
+float update_level(size_t lvl) {
+    const float a = 0.1f;
+    const float b = 0.1f;
+    float x = (float) lvl / PERC_MAX_LVL;
+    if (x < b) {
+        return x*a/b;
+    } else if (x >= b && x < 1-b) {
+        return (1-2*a)/(1-2*b)*(x-b)+a;
+    } else {
+        return a/b*(x-1+b)+1-a;
+    }
+}
+
+int update_callback(int pause, uint32_t* pixels_in, struct Context* cx) {
+    const size_t width = WORLD_WIDTH;
+    const size_t height = WORLD_HEIGHT;
+    float* restrict site_prob = cx->site_prob;
+    uint32_t* restrict color = cx->color;
+    uint32_t* restrict pixels = pixels_in;
+    DsufNode* uf = cx->uf;
+
+    float lvl = update_level(cx->level);
+    
+    for (size_t y = 1; y < height-1; ++y) {
+        for (size_t x = BLOCKED_WIDTH; x < WORLD_WIDTH - BLOCKED_WIDTH; ++x) {
+            if (site_prob[x+y*width] < lvl) {
+                DsufNode* restrict c = &uf[x+y*width];
+                DsufNode* restrict r = &uf[(x+1)+y*width];
+                DsufNode* restrict l = &uf[(x-1)+y*width];
+                DsufNode* restrict t = &uf[x+(y-1)*width];
+                DsufNode* restrict b = &uf[x+(y+1)*width];
+               
+                if (site_prob[x+y*width] < lvl) {
+                    if (color[x+y*width] == 0xff000000) color[x+y*width] = 0xffffffff;
+                }
+                if (site_prob[x+1+y*width] < lvl) {
+                    if (color[x+1+y*width] == 0xff000000) color[x+1+y*width] = 0xffffffff;
+                }
+                if (site_prob[x-1+y*width] < lvl) {
+                    if (color[x-1+y*width] == 0xff000000) color[x-1+y*width] = 0xffffffff;
+                }
+                if (site_prob[x+(y-1)*width] < lvl) {
+                    if (color[x+(y-1)*width] == 0xff000000) color[x+(y-1)*width] = 0xffffffff;
+                }
+                if (site_prob[x+(y+1)*width] < lvl) {
+                    if (color[x+(y+1)*width] == 0xff000000) color[x+(y+1)*width] = 0xffffffff;
+                }
+
+                if (site_prob[x+1+y*width] < lvl) {
+                    uint32_t newcolor = (color[ds_find(c)-uf] & color[ds_find(r)-uf]);
+                    DsufNode* newnode = ds_union(c, r);
+                    color[newnode-uf] = newcolor;
+                }
+                if (site_prob[x-1+y*width] < lvl) {
+                    uint32_t newcolor = (color[ds_find(c)-uf] & color[ds_find(l)-uf]);
+                    DsufNode* newnode = ds_union(c, l);
+                    color[newnode-uf] = newcolor;
+                }
+                if (site_prob[x+(y-1)*width] < lvl) {
+                    uint32_t newcolor = (color[ds_find(c)-uf] & color[ds_find(t)-uf]);
+                    DsufNode* newnode = ds_union(c, t);
+                    color[newnode-uf] = newcolor;
+                }
+                if (site_prob[x+(y+1)*width] < lvl) {
+                    uint32_t newcolor = (color[ds_find(c)-uf] & color[ds_find(b)-uf]);
+                    DsufNode* newnode = ds_union(c, b);
+                    color[newnode-uf] = newcolor;
+                }
+            }
+        }
+    }
+    
+#ifdef PP_UNIX
+    size_t n = width*height;
+
+    for (size_t i = 0; i < THREAD_COUNT; ++i) {
+        (void)sem_post(&cx->thread_args[i].semaphore_in);
+    }
+
+    const size_t chunk_size = n / (THREAD_COUNT+1);
+
+    uint32_t* px = &pixels[chunk_size*THREAD_COUNT];
+    DsufNode* ufp = &uf[chunk_size*THREAD_COUNT];
+    for (; px < pixels+n; ++px, ++ufp) {
+        DsufNode* node = ds_lazy_find(ufp); 
+        uint32_t* restrict pixel = px;
+        uint32_t* restrict col = &color[node - uf];
+        assign_3_4(pixel, *col);
+    }
+ 
+    for (size_t i = 0; i < THREAD_COUNT; ++i) {
+        CALL_RETRY(sem_wait(&cx->thread_args[i].semaphore_out));
+    }
+     
+#else
+
+    uint32_t* px = pixels;
+    DsunNode* ufp = uf;
+    for (; px < pixels+width*height; ++px, ++ufp) {
+        DsufNode* node = ds_lazy_find(ufp); 
+        uint32_t* restrict pixel = px;
+        uint32_t* restrict col = &color[node - uf];
+        assign_3_4(pixel, *col);
+    }
+
+#endif
+
+    if (cx->level % 10 == 0) {
+        printf("%f\n", lvl);
+        fflush(stdout);
+    }
+
+    if (!pause)
+        ++cx->level;
+    return 1;
+}
